929060aa6ce3fd01370c88bcee9bc442e8bc55f1
djf99 <djf99@uclive.ac.nz>
2020-10-06 15:20:10 +1300

    Added paddle to makefile and fixed ifndef statement

--- paddle.c
+++ paddle.c
@@ -1,3 +1,4 @@
+#include "system.h"
 
 #define PADDLE_LENGTH 2
 #define MIN_POSITION 0
@@ -6,6 +7,6 @@ static uint8_t paddle_position = 0;
 
 static uint8_t get_paddle (void)
 {
-    return (1 << paddle_position) | (1 << (paddle_position + 1);
+    return (1 << paddle_position) | (1 << (paddle_position + 1));
 
 }
