986aa40c83d47f05ad2164620c1d99b1be873be2
Noah Irving <nji29@uclive.ac.nz>
2020-10-06 15:22:57 +1300

    get_paddle now works with different lengths of paddles

--- paddle.c
+++ paddle.c
@@ -6,6 +6,10 @@ static uint8_t paddle_position = 0;
 
 static uint8_t get_paddle (void)
 {
+    uint8_t paddle = 0;
+    for (uint8_t i = 0; i < PADDLE_LENGTH; i++) {
+        paddle |= (1 << (paddle_position + i));
+    }
     return (1 << paddle_position) | (1 << (paddle_position + 1);
 
 }
