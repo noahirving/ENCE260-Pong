afea392aba3632214bd06770e209f7ebeaa4e52f
djf99 <djf99@uclive.ac.nz>
2020-10-07 17:40:47 +1300

    Added check for ball hitting paddle

--- ball.c
+++ ball.c
@@ -1,1 +1,10 @@
+#include "paddle.h"
 
+static uint8_t ball_col;
+static uint8_t ball_row;
+
+int check_ball_hit (void)
+{
+    uint8_t paddle = get_paddle;
+    return (ball_row & (paddle & (1 << ball_row)));
+}
